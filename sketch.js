d78ecd53dae3310891d9fad6a59c9e026610a2a3
hubertYumengLu <72938590+hubertYumengLu@users.noreply.github.com>
2020-10-28 11:03:57 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,14 +8,73 @@ function setup() {
 // time, the sketch is a static image. If it draws something different on
 // different frames, the sketch is an animation.
 function draw() {
-  background(100,0,100);
+  background(80,100,30);
   noStroke()
   fill('white')
   rect(50, 20, 500,20)
-  part1()  
+  part1()
+  part2()
+  part3()
+  part4()
+  part5()
+  part6()
+}
+function part1(){
+  let brightness1 = 20
+  fill(20, 100, brightness1)
+  for (let i = 0; i <= 4; i++){
+  circle(width * 1 / 9 + 30 *i, height * 2/3 + 20 * i, 30)
+  }
+  for (let i = 0; i <= 4; i++){
+    circle(width * 2 / 9 + 20 *i, height * 4/7 + 25 * i, 30)
+  }
 }
 
-function part1(){
-  fill(0, 0, 100)
-  circle(width * 1 / 5, height / 2, 150)
+function part2(){
+  let brightness2 = 20
+  fill(40, 100, brightness2)
+  for (let i = 0; i <= 4; i++){
+  circle(width * 5 / 14 + 10 *i, height * 1/2 + 30 * i, 30)
+  }
+  for (let i = 0; i <= 4; i++){
+    circle(width * 5 / 11 +  3 * i, height * 48/100 + 30 * i, 30)
+  }
+}
+
+function part3(){
+  let brightness3 = 20
+  fill(60, 100, brightness3)
+  for (let i = 0; i <= 4; i++){
+  circle(width * 9 / 14 - 10 * i, height * 1/2 + 30 * i, 30)
+  }
+  for (let i = 0; i <= 4; i++){
+    circle(width * 6 / 11 -  3 * i, height * 48/100 + 30 * i, 30)
+  }
+}
+
+function part4(){
+  let brightness4 = 20
+  fill(5, 100, brightness4)
+  for (let i = 0; i <= 4; i++){
+  circle(width * 8 / 9 - 30 *i, height * 2/3 + 20 * i, 30)
+  }
+  for (let i = 0; i <= 4; i++){
+    circle(width * 7 / 9 - 20 *i, height * 4/7 + 25 * i, 30)
+  }
+}
+
+function part5(){
+  let brightness5 = 20
+  fill(0, 0, brightness5)
+  for (let i = 0; i <= 15; i++){
+  circle(50 + 33.33 * i, 150, 30)
+  }
+}
+
+function part6(){
+  let brightness6 = 20
+  fill(12, 100, brightness6)
+  for (let i = 0; i <= 15; i++){
+  circle(50 + 33.33 * i, 100, 30)
+  }
 }
