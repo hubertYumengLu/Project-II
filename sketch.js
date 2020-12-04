d4bf128189e2eb6f10ed973a5b10e5922629a1aa
hubertYumengLu <72938590+hubertYumengLu@users.noreply.github.com>
2020-12-04 12:45:38 +0800

    JSON

--- sketch.js
+++ sketch.js
@@ -2,63 +2,65 @@ let i = 1
 //judgment on whether to play or pause audio
 let brightness = [200, 200, 200, 200, 200, 200, 1000]
 //first six values are brightness of circles; last value is brightness of playPauseButton
-let vln1, vln2, vla, cb, wnd, harp
 let audio = []
+let music
 let button;
-let sound1Gain, sound2Gain, sound3Gain, sound4Gain, sound5Gain, sound6Gain, masterGain;
+// let sound1Gain, sound2Gain, sound3Gain, sound4Gain, sound5Gain, sound6Gain, masterGain;
+let tutorial = []
+let parts, instruction
+let handMovement = loadImage("assets/images/handMovement.gif")
+
 
 function preload(){
+  parts = loadJSON("parts.json")
+  instruction = loadJSON("tutorial.json")
   soundFormats("mp3")
-  vln1 = loadSound("assets/sounds/Barcarole (Violin I).mp3")
-  console.info(vln1)
-  audio.push(vln1)
-  vln2 = loadSound("assets/sounds/Barcarole (Violin II).mp3")
-  audio.push(vln2)
-  vla = loadSound("assets/sounds/Barcarole (Viola).mp3")
-  audio.push(vla)
-  cb = loadSound("assets/sounds/Barcarole (Cello & Bass).mp3")
-  audio.push(cb)
-  wnd = loadSound("assets/sounds/Barcarole (Woodwinds).mp3")
-  audio.push(wnd)
-  harp = loadSound("assets/sounds/Barcarole (Harp & Triangle).mp3")
-  audio.push(harp)
+  music = loadSound("assets/sounds/Guillaume Tell.mp3")
+  console.info(music)
+  audio.push(music)
 }
 
 function setup() {
-  let cnv = createCanvas(600, 400);
+  let cnv = createCanvas(1200, 720);
   colorMode(HSB, 1000)
-  cnv.mousePressed(startSound);
-  // Conjoining all Audio Files 
-  masterGain = new p5.Gain()
-  masterGain.connect()
-  audio[0].disconnect()
-  sound1Gain = new p5.Gain()
-  sound1Gain.setInput(audio[0])
-  sound1Gain.connect(masterGain)
-  audio[1].disconnect()
-  sound2Gain = new p5.Gain()
-  sound2Gain.setInput(audio[1])
-  sound2Gain.connect(masterGain)
-  audio[2].disconnect()
-  sound3Gain = new p5.Gain()
-  sound3Gain.setInput(audio[2])
-  sound3Gain.connect(masterGain)
-  audio[3].disconnect()
-  sound4Gain = new p5.Gain()
-  sound4Gain.setInput(audio[3])
-  sound4Gain.connect(masterGain)
-  audio[4].disconnect()
-  sound5Gain = new p5.Gain()
-  sound5Gain.setInput(audio[4])
-  sound5Gain.connect(masterGain)
-  audio[5].disconnect()
-  sound6Gain = new p5.Gain()
-  sound6Gain.setInput(audio[5])
-  sound6Gain.connect(masterGain)
+  // cnv.mousePressed(startSound);
 }
 
 function draw() {
   background(800,1000,330);
+  // load tutorial
+  rect(instruction.window[0].x, instruction.window[0].y, instruction.window[0].w, instruction.window[0].h)
+  tutorial.push(instruction.window)
+  push()
+  textSize(instruction.text[0].size)
+  fill(instruction.text[0].color)
+  text(instruction.text[0].text, instruction.text[0].x, instruction.text[0].y)
+  tutorial.push(instruction.text[0])
+  pop()
+  
+  push()
+  textSize(instruction.text[1].size)
+  fill(instruction.text[1].color)
+  text(instruction.text[1].text, instruction.text[1].x, instruction.text[1].y)
+  tutorial.push(instruction.text[1])
+  pop()
+  
+  push()
+  textSize(instruction.text[2].size)
+  fill(instruction.text[2].color)
+  text(instruction.text[2].text, instruction.text[2].x, instruction.text[2].y)
+  tutorial.push(instruction.text[2])
+  pop()
+  
+  push()
+  textSize(instruction.text[3].size)
+  textStyle(ITALIC)
+  fill(instruction.text[3].color)
+  text(instruction.text[3].text, instruction.text[3].x, instruction.text[3].y)
+  tutorial.push(instruction.text[3])
+  pop()
+
+  //load interface if mouse pressed on canvas
   noStroke()
   fill('white')
   rect(50, 20, 500,20)
@@ -69,6 +71,8 @@ function draw() {
   part4()
   part5()
   part6()
+
+
   // Volume by Mouse Mechanics 
   let sound1Volume = constrain(map(mouseX,width,0,0,1), 0, 1);
   let sound2Volume = 1-sound1Volume;
@@ -78,6 +82,7 @@ function draw() {
   sound4Gain.amp(sound2Volume);
   let masterVolume = constrain(map(mouseY,height,0,0,1), 0, 1);
   masterGain.amp(masterVolume);
+
   // UI Text based on Mouse Mechanics
   fill(0, 0, 1000)
   text('◄◄◄ Vln1&2 Vol ♫', width/2 - 32, height - 15)
@@ -154,26 +159,26 @@ function part6(){
 function playPauseButton(){
   fill(0,0,brightness[7])
   rect(275,350,50,30)
-  fill(120,1000,brightness[7])
-  triangle(285,355,285,375,315,365)
+
 }
 
 // Sound Calls (Start and Stop)
+// function startSound() {
+//   audio[0].loop();
+//   audio[1].loop();
+//   audio[2].loop();
+//   audio[3].loop();
+//   audio[4].loop();
+//   audio[5].loop();
+//   loop();
+// }
+// function mouseReleased() {
+//   audio[0].pause();
+//   audio[1].pause();
+//   audio[2].pause();
+//   audio[3].pause();
+//   audio[4].pause();
+//   audio[5].pause();
+// }
 
-function startSound() {
-  audio[0].loop();
-  audio[1].loop();
-  audio[2].loop();
-  audio[3].loop();
-  audio[4].loop();
-  audio[5].loop();
-  loop();
-}
-function mouseReleased() {
-  audio[0].pause();
-  audio[1].pause();
-  audio[2].pause();
-  audio[3].pause();
-  audio[4].pause();
-  audio[5].pause();
-}
+
